b95ce08c13fb9dad65cea37236b6ff5698ad1a32
Jeremy Kuehn <100542455+arkterra90@users.noreply.github.com>
2025-02-08 08:36:20 -0600

    completes 6_array excercise

--- ruby_basics/6_arrays/exercises/array_exercises.rb
+++ ruby_basics/6_arrays/exercises/array_exercises.rb
@@ -1,24 +1,30 @@
 def nil_array(number)
   # return an array containing `nil` the given number of times
+  Array.new(number, nil)
 end
 
 def first_element(array)
   # return the first element of the array
+  array[0]
 end
 
 def third_element(array)
   # return the third element of the array
+  array[2]
 end
 
 def last_three_elements(array)
+  array.last(3)
   # return the last 3 elements of the array
 end
 
 def add_element(array)
+  array << 3
   # add an element (of any value) to the array
 end
 
 def remove_last_element(array)
+  array.pop
   # Step 1: remove the last element from the array
 
   # Step 2: return the array (because Step 1 returns the value of the element removed)
@@ -27,6 +33,7 @@ end
 
 def remove_first_three_elements(array)
   # Step 1: remove the first three elements
+  array.shift(3)
 
   # Step 2: return the array (because Step 1 returns the values of the elements removed)
   array
@@ -34,28 +41,36 @@ end
 
 def array_concatenation(original, additional)
   # return an array adding the original and additional array together
+  original.concat(additional)
 end
 
 def array_difference(original, comparison)
   # return an array of elements from the original array that are not in the comparison array
+  new_array = original - comparison
+  new_array
 end
 
 def empty_array?(array)
   # return true if the array is empty
+  array.empty?
 end
 
 def reverse(array)
+  array.reverse
   # return the reverse of the array
 end
 
 def array_length(array)
+  a = array.count
   # return the length of the array
 end
 
 def include?(array, value)
   # return true if the array includes the value
+  array.include?(value)
 end
 
 def join(array, separator)
   # return the result of joining the array with the separator
+  array.join(separator)
 end
